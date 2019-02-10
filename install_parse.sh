4142c6f5822a2bd0946896f5fb1708b41c9f0636
looneytkp <sgm4kv@gmail.com>
2019-02-10 15:47:06 +0000

    fix for reinstall

--- install_parse.sh
+++ install_parse.sh
@@ -54,7 +54,7 @@ else
 		y=$(cat "$directory"/install_parse.sh "$directory"/changelog "$directory"/.help|md5sum)
 		if [ "$x" != "$y" ];then
 			cp -u {install_parse.sh,changelog,.help} "$directory"
-			echo -e "components updated.\\n$name: up-to-date -- $version.";exit 0
+			echo -e "components updated.\\n$name: up-to-date -- $version.";return 5
 		else
 			echo -e "$name: up-to-date -- $version."
 		fi
